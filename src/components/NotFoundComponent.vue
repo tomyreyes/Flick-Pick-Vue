d6f8ac7497de66a6d5998a6ba24e510c9fc15a56
Tomy <tomyleereyes@gmail.com>
2018-07-22 17:51:12 -0700

    temp removing history mode

---
+++ src/components/NotFoundComponent.vue
@@ -0,0 +1,8 @@
+<template>
+	<h1>Hi</h1>
+</template>
+<script>
+export default {
+	name: 'NotFoundComponent'
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import App from './App.vue'
 import About from './components/About.vue'
 import FeaturedMovies from './components/FeaturedMovies.vue'
 import MyList from './components/MyList.vue'
+import NotFoundComponent from './components/NotFoundComponent.vue'
 
 Vue.use(Vuetify)
 Vue.use(VueRouter)
@@ -14,12 +15,13 @@ Vue.config.productionTip = false
 const routes = [
 	{ path: '/mylist', component: MyList },
 	{ path: '/about', component: About },
-	{ path: '/', component: FeaturedMovies }
+	{ path: '/', component: FeaturedMovies },
+	{ path: '*', component: NotFoundComponent }
 ]
 
 const router = new VueRouter({
 	routes,
-	mode: 'history'
+	// mode: 'history'
 })
 
 new Vue({
